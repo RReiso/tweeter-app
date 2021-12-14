114fbcd43bcd6d6b666b6d24f7c29a270abcc823
Ruta Reisoglu <rutareiso@protonmail.com>
2021-12-14 03:52:12 +0000

    Update counter color depending on remaining characters

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
-  let counter = 140;
   $("#tweet-text").on("keyup", function(e) {
-    // $(".counter").text(counter--);
-    console.log($(this).val().length);
     const textLength = $(this).val().length;
-    $(this).parent().siblings(".tweet-submit").children(".counter").text(140 - textLength);
+    const remainingChars = 140 - textLength;
+    const counter = $(this).parent().siblings(".tweet-submit").children(".counter");
+    counter.text(remainingChars);
+    remainingChars < 0 ?  counter.addClass("red") : counter.removeClass("red");
   });
 });
