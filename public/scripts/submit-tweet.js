1087f5ed24ffb1528968a2d154f9329fb853429f
Ruta Reisoglu <rutareiso@protonmail.com>
2021-12-14 21:34:05 +0000

    Validate form input data befor making an AJAX POST request

--- public/scripts/submit-tweet.js
+++ public/scripts/submit-tweet.js
@@ -1,7 +1,23 @@
 $(document).ready(function() {
   $(".tweet-form").on("submit", function(e) {
     e.preventDefault();
-    let formData = $(this).serialize();
+    const formData = $(this).serialize();
+    // console.log(formData);
+    const validData = formData.slice(5);
+    const regex = /%20/g;
+    const isEmptyString = validData.replace(regex, "") === "";
+    // console.log(isEmptyString);
+
+    if (!validData || isEmptyString) {
+      alert("Tweet cannot be empty!");
+      return;
+    }
+
+    if (validData.length > 140) {
+      alert("Tweet is too long!");
+      return;
+    }
+
     $.ajax('/tweets', { method: 'POST', data: formData });
   });
 });
