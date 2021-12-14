bc90cbc4253603d56d3ee329f1d59d9f95d9518a
Ruta Reisoglu <rutareiso@protonmail.com>
2021-12-14 18:25:58 +0000

    Rename variables

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
   $("#tweet-text").on("keyup", function(e) {
-    const textLength = $(this).val().length;
-    const remainingChars = 140 - textLength;
-    const counter = $(this).parent().siblings(".tweet-submit").children(".counter");
-    counter.text(remainingChars);
-    remainingChars < 0 ?  counter.addClass("red") : counter.removeClass("red");
+    const $textLength = $(this).val().length;
+    const $remainingChars = 140 - $textLength;
+    const $counter = $(this).parent().siblings(".tweet-submit").children(".counter");
+    $counter.text($remainingChars);
+    $remainingChars < 0 ?  $counter.addClass("red") : $counter.removeClass("red");
   });
 });
