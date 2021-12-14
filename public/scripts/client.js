df2afbd28ec83b00f7852bcb4b97cfe40bf2a056
Ruta Reisoglu <rutareiso@protonmail.com>
2021-12-14 20:22:18 +0000

    Make AJAX GET request and load tweets

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,62 +5,41 @@
  */
 
 $(document).ready(function() {
-  const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ];
+  const createTweetElement = (tweetData) => {
+    const $tweet = `<article class="tweet">
+    <header>
+    <div class="tweet-author">
+    <div>
+    <img src="${tweetData.user.avatars}" alt="tweet-author-icon">
+    <span class="tweet-author-name">${tweetData.user.name}</span>
+    </div>
+    <span class="tweet-author-handle">${tweetData.user.handle}</span>
+    </div>
+    <p class="tweet-text">${tweetData.content.text}</p>
+    </header>
+    <footer>
+    <span>${timeago.format(tweetData.created_at)}</span>
+    <div class="tweet-icons">
+    <i class="fas fa-flag"></i>
+    <i class="fas fa-retweet"></i>
+    <i class="fas fa-heart"></i>
+    </div>
+    </footer>
+    </article>`;
+    return $tweet;
+  };
 
+  const loadTweets = () => {
+    $.ajax(' http://localhost:8080/tweets', { method: 'GET' })
+      .then(tweets => renderTweets(tweets));
+  };
+  
   const renderTweets = (tweets) => {
     for (let tweet of tweets) {
       let newTweet = createTweetElement(tweet);
       $('.all-tweets').append(newTweet);
     }
   };
-
-  const createTweetElement = (tweetData) => {
-    const $tweet = `<article class="tweet">
-    <header>
-      <div class="tweet-author">
-        <div>
-          <img src="${tweetData.user.avatars}" alt="tweet-author-icon">
-          <span class="tweet-author-name">${tweetData.user.name}</span>
-        </div>
-      <span class="tweet-author-handle">${tweetData.user.handle}</span>
-    </div>
-      <p class="tweet-text">${tweetData.content.text}</p>
-    </header>
-    <footer>
-      <span>${timeago.format(tweetData.created_at)}</span>
-      <div class="tweet-icons">
-        <i class="fas fa-flag"></i>
-        <i class="fas fa-retweet"></i>
-        <i class="fas fa-heart"></i>
-      </div>
-    </footer>
-  </article>`;
-    return $tweet;
-  };
-
-  renderTweets(data);
   
+  loadTweets();
 });
